e2bac67d15fa6e2dfa251778a370ecb380157c7d
Caleb Ziegler <caleb.ziegler@gmail.com>
2021-02-13 13:02:58 -0600

    Updates App.vue to have navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       color="grey darken-4"
       dark
     >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title>
             <router-link to="/" tag="span" style="cursor: pointer">
                 Pat Mac Copper Works
@@ -15,10 +15,33 @@
 
         <v-btn text to="/" router>Home</v-btn> 
         <v-btn text to="/about" router>About</v-btn> 
-        <v-btn text to="/gallery" router>Gallery</v-btn> 
         <v-btn text to="/blog" router>Blog</v-btn> 
+        <v-btn text to="/gallery" router>Gallery</v-btn> 
 
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer"
+                         absolute
+                         temporary>
+        <v-list nav dense>
+            <v-list-item-group v-model="group">
+                <v-list-item to="/">
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item>
+                <v-list-item to="/about">
+                    <v-list-item-title>About</v-list-item-title>
+                </v-list-item>
+                <v-list-item to="/blog">
+                    <v-list-item-title>Blog</v-list-item-title>
+                </v-list-item>
+                <v-list-item to="/gallery">
+                    <v-list-item-title>Gallery</v-list-item-title>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+
+    </v-navigation-drawer>
+
 
     <v-main>
         <router-view></router-view>
@@ -32,7 +55,7 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false, 
   }),
 };
 </script>
