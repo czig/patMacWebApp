3779865db82855e5f42b2b833db30010775fab38
Caleb Ziegler <caleb.ziegler@gmail.com>
2021-02-13 12:50:42 -0600

    Updates home page to only have image and some text

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,20 @@
 <template>
-  <v-container>
-        <v-carousel height="600">
-            <v-carousel-item src="../assets/working_maple.jpg"></v-carousel-item>
-            <v-carousel-item src="../assets/torch.jpg"></v-carousel-item>
-            <v-carousel-item src="../assets/patrick.jpg"></v-carousel-item>
-        </v-carousel>
+  <v-container fluid class="ma-0 pa-0">
+      <v-img max-width="100%"
+             :aspect-ratio="20/6" 
+             src="../assets/pat_mac_sign.jpg">
+      </v-img>
+      <v-container>
+          <v-row class="mt-3">
+              <v-col>
+                  <span class="text-h5 text-sm-h4 text-md-h2">Welcome to PatMacCopperWorks! </span>
+                  <p class="text-justify mt-4">
+                      I'm Patrick McGowan, and this is website is devoted to my copper art. Please check out the "About" section to read about how I got started on copper art. I will be posting in the "Blog" page as I create more copper pieces. Feel free to look at the gallery to see images of my finished products and me at work! Thanks for visiting! 
+                  </p>
+                  <p class="text-justify">-PM</p>
+              </v-col>
+          </v-row>
+      </v-container>
   </v-container>
 </template>
 
@@ -20,3 +30,10 @@
     }),
   }
 </script>
+
+<style>
+    .parallax {
+        background-attachment: fixed;
+        background-size: contain;
+    }
+</style>
