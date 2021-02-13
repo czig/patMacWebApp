f479406aadc31862c08e501064b16997cc6ea609
Caleb Ziegler <caleb.ziegler@gmail.com>
2021-02-13 17:14:55 -0600

    Fixes typo in home page

--- src/components/home.vue
+++ src/components/home.vue
@@ -9,7 +9,7 @@
               <v-col>
                   <span class="text-h5 text-sm-h4 text-md-h2">Welcome to PatMacCopperWorks! </span>
                   <p class="text-justify mt-4">
-                      I'm Patrick McGowan, and this is website is devoted to my copper art. Please check out the "About" section to read about how I got started on copper art. I will be posting in the "Blog" page as I create more copper pieces. Feel free to look at the gallery to see images of my finished products and me at work! Thanks for visiting! 
+                      I'm Patrick McGowan, and this website is devoted to my copper art. Please check out the "About" section to read about how I got started on copper art. I will be posting in the "Blog" page as I create more copper pieces. Feel free to look at the gallery to see images of my finished products and me at work! Thanks for visiting! 
                   </p>
                   <p class="text-justify">-PM</p>
               </v-col>
