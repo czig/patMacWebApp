30b7f12cb2eb76c437f987e7de960b90ef121d2b
Caleb Ziegler <caleb.ziegler@gmail.com>
2022-08-08 16:31:52 -0600

    Fixes sizing of patrick picture in about

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,11 +7,12 @@
               </v-col>
           </v-row>
           <v-row>
-              <v-col class="col-xs-12 col-md-4">
-                <img src="../assets/patrick.jpg"
-                 :width="360">
+              <v-col class="col-xs-12 col-sm-6 col-md-4">
+                <v-img src="../assets/patrick.jpg"
+                       :width="$vuetify.breakpoint.width < 600 ? $vuetify.breakpoint.width - 60 : 360"
+                ></v-img>
               </v-col>
-              <v-col class="col-xs-12 col-md-8">
+              <v-col class="col-xs-12 col-sm-6 col-md-8">
                   <p class="text-justify">
                     Hi!  My name is Patrick McGowan.  I live in Massachusetts.  I am 23 years old and have been making Copper designs since October 2019.  I have had Epilepsy since I was a small child.  My Dad and I were brainstorming and thought why not use some of the skills I learned in high School.  I work with an Oxy-Acetylene torch, I braze and solder large designs and use some power tools in our Garage shop.  My Dad and I share ideas and come up with different designs.  I have a part time job and do this on the side.  My skills continue to improve as I learn.  I have photos of my designs on this web site, and I am willing to consider new designs or requests.  I finish and clear coat all my designs, so they do not tarnish. 
                   </p>
